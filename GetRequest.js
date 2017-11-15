5a8d752133f514a08bf29042f6d5e82bc59f87a1
Johan Peeters <yo@johanpeeters.com>
2017-11-15 21:11:22 +0100

    factor making the request out of the subcommands

---
+++ GetRequest.js
@@ -0,0 +1,49 @@
+const https = require('https')
+const http = require('http')
+
+const makeGetRequest = (config) => {
+  const {protocol, host, path, successMsg, failureMsg, expectedContent} = config
+  return new Promise(function(resolve, reject){
+    const options = {
+      hostname: host,
+      method: 'GET',
+      path: path,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+    let scheme = protocol === 'http'?http:https
+    const req = scheme.request(options, (res) => {
+      res.setEncoding('utf8')
+      let rawData = ''
+      res.on('data', (chunk) => {rawData += chunk})
+      res.on('end', () => {
+        const statusCode = res.statusCode
+        if (statusCode == 200) {
+          if (expectedContent) {
+            if (expectedContent != rawData) {
+                let err = new Error(`expected at https://${host}/${path}: ${expectedContent}`)
+                err.actualContent = rawData
+                reject(err)
+            }
+          }
+          resolve(successMsg)
+        } else {
+          let err = new Error(failureMsg)
+          err.statusCode = statusCode
+          err.rawData = rawData
+          reject(err)
+        }
+      })
+    })
+    req.on('error', (e) => {
+      console.error(`${failureMsg}\n\t${e.message}`)
+      reject(e)
+    })
+    req.end()
+  })
+}
+
+module.exports = {
+  makeGetRequest: makeGetRequest
+}
